f19dfbaa6e5c78af604d95125707bdb7589ed755
FedorBuggins <fedorbuggins@gmail.com>
2025-01-12 15:11:25 +0500

    feat: dirty vk, tg impl
    vk token doesn't work

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,72 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Injectable, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Telegraf } from 'telegraf';
+import { VK } from 'vk-io';
+
+export interface PublishedPost extends NewPost {
+  id: string;
+  url: string;
+}
+
+export interface NewPost {
+  title: string;
+  description: string;
+  text: string;
+  tags: string[];
+  links: string[];
+}
+
+export interface PostPlatform {
+  id: string;
+  publish(post: NewPost): Promise<PublishedPost>;
+}
+
+@Injectable()
+export class PostPlatformArray extends Array<PostPlatform> {
+  constructor() {
+    super();
+    const TG_TOKEN = process.env.TG_TOKEN ?? panic('No TG_TOKEN');
+    const VK_TOKEN = process.env.VK_TOKEN ?? panic('No VK_TOKEN');
+    const tg = new Telegraf(TG_TOKEN).telegram;
+    this.push({
+      id: 'tg',
+      async publish(post): Promise<PublishedPost> {
+        const channelId = '@justerest_crossposting_channel';
+        return (await tg.sendMessage(channelId, JSON.stringify(post))) as any;
+      },
+    });
+    this.push({
+      id: 'vk',
+      async publish(post): Promise<PublishedPost> {
+        const vk = new VK({ token: VK_TOKEN });
+        const message = JSON.stringify(post);
+        return (await vk.api.wall.post({ message })) as any;
+      },
+    });
+  }
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly platforms: PostPlatformArray,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('post')
+  async post(post: NewPost): Promise<void> {
+    const r = await Promise.allSettled(
+      this.platforms.map((p) => p.publish(post)),
+    );
+    console.log(r);
+  }
+}
+
+function panic(err: string): never {
+  throw new Error(err);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppController } from './app.controller';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT ?? 3000);
+  await app.get(AppController).post({
+    title: 'test',
+    description: '',
+    text: '',
+    tags: [],
+    links: [],
+  });
+  await app.close();
 }
 
 bootstrap();
